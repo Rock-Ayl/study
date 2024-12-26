95bf1d3ec6c15faedc846e7f6a922166f7e1d739
Rock-Ayl <AnYongLiang4869@163.com>
2024-12-26 16:21:19 +0800

    修改：整理

--- dubbo/src/main/java/com/rock/dubbo/adaptive/java/Start.java
+++ dubbo/src/main/java/com/rock/dubbo/adaptive/java/Start.java
@@ -1,6 +1,7 @@
 package com.rock.dubbo.adaptive.java;
 
 import com.rock.dubbo.adaptive.java.bo.Wheel;
+import com.rock.dubbo.adaptive.java.maker.car.CarMaker;
 import com.rock.dubbo.adaptive.java.maker.wheel.WheelMaker;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 
@@ -20,6 +21,10 @@ public class Start {
         //模拟url
         URL url = new URL("http://192.168.0.101:20880/XxxService?wheel.maker=MichelinWheelMaker");
 
+        /**
+         * 代理 spi
+         */
+
         //载入
         ExtensionLoader<WheelMaker> wheelMakerExtensionLoader = ExtensionLoader.getExtensionLoader(WheelMaker.class);
         //获取代理
@@ -29,11 +34,16 @@ public class Start {
         //输出
         System.out.println(wheel);
 
-        //todo 制造赛车,暂时没有注入 setWheelMaker
-        //ExtensionLoader<CarMaker> carMakerExtensionLoader = ExtensionLoader.getExtensionLoader(CarMaker.class);
-        //获取赛车实例
-        //CarMaker carMaker = carMakerExtensionLoader.getExtension("RaceCarMaker");
-        //carMaker.makeCar(url);
+        /**
+         * 代理 spu + 注入
+         * //todo 制造赛车,暂时没有注入 setWheelMaker
+         */
+
+        //载入赛车
+        ExtensionLoader<CarMaker> carMakerExtensionLoader = ExtensionLoader.getExtensionLoader(CarMaker.class);
+
+        //todo 没有工厂，获取赛车实例 CarMaker carMaker = carMakerExtensionLoader.getExtension("RaceCarMaker");
+        //实现 carMaker.makeCar(url);
 
     }
 
