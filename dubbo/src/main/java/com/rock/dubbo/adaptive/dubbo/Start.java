766783d1c2b09511f346ec778c59c6c7fbf31731
Rock-Ayl <AnYongLiang4869@163.com>
2024-12-26 17:32:06 +0800

    修改：spi 自适应扩展-13

--- dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Start.java
+++ dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Start.java
@@ -12,15 +12,25 @@ public class Start {
     public static void main(String[] args) {
         //载入
         ExtensionLoader<Animal> loader = ExtensionLoader.getExtensionLoader(Animal.class);
-        //获取自适配扩展
-        Animal animal = loader.getAdaptiveExtension();
+
+        /**
+         * spi 实现
+         */
+
+        //非代理 spi 实现
+        loader.getExtension("cat").eat(null);
+        loader.getExtension("dog").eat(null);
+
+        /**
+         * spi + 自适配扩展 实现
+         */
 
         //没有值，默认dog
-        animal.eat(URL.valueOf("http://localhost:9999/xxx"));
+        loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx"));
         //匹配正确，加载cat
-        animal.eat(URL.valueOf("http://localhost:9999/xxx?animalType=cat"));
+        loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx?animalType=cat"));
         //匹配另一个字段，加载cat
-        animal.eat(URL.valueOf("http://localhost:9999/xxx?animalType2=cat"));
+        loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx?animalType2=cat"));
 
     }
 
