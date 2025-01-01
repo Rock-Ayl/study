c1d61c12bf66ce7f7628e7d29f33f053d56b1140
Rock-Ayl <AnYongLiang4869@163.com>
2025-01-01 23:27:13 +0800

    新增：javassist 学习-4

--- jdk/src/main/java/com.rock.jdk/javassist/JavassistExample.java
+++ jdk/src/main/java/com.rock.jdk/javassist/JavassistExample.java
@@ -12,21 +12,26 @@ public class JavassistExample {
 
     public static void main(String[] args) throws Exception {
 
+        /**
+         * 将类加载到内存中
+         */
+
         //创建一个 ClassPool
         ClassPool pool = ClassPool.getDefault();
-
         //创建一个新的类
         CtClass cc = pool.makeClass("com.rock.jdk.javassist.out.MyDynamicClass");
-
         //创建一个新的方法
         CtMethod method = CtMethod.make("public String sayHello(String name) { return \"Hello, \" + name;}", cc);
-
         //将方法添加到类中
         cc.addMethod(method);
 
         /**
          * 如果有需要,将类写入指定目录
          * cc.writeFile("javassist/out");
+         */
+
+        /**
+         * 反射出对应实例、调用方法
          */
 
         //加载类
