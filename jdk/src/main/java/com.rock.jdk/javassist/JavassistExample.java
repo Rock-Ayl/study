6fffcf0807190db35d7fc1afd9a9fa8be314952c
Rock-Ayl <AnYongLiang4869@163.com>
2025-01-01 23:24:40 +0800

    新增：javassist 学习-2

--- jdk/src/main/java/com.rock.jdk/javassist/JavassistExample.java
+++ jdk/src/main/java/com.rock.jdk/javassist/JavassistExample.java
@@ -12,33 +12,32 @@ public class JavassistExample {
 
     public static void main(String[] args) throws Exception {
 
-        // 创建一个 ClassPool
+        //创建一个 ClassPool
         ClassPool pool = ClassPool.getDefault();
 
-        // 创建一个新的类
-        CtClass cc = pool.makeClass("com.example.MyDynamicClass");
+        //创建一个新的类
+        CtClass cc = pool.makeClass("com.rock.jdk.javassist.out.MyDynamicClass");
 
-        // 创建一个新的方法
-        CtMethod method = CtMethod.make(
-                "public String sayHello(String name) { " +
-                        "return \"Hello, \" + name;" +
-                        "}", cc);
+        //创建一个新的方法
+        CtMethod method = CtMethod.make("public String sayHello(String name) { return \"Hello, \" + name;}", cc);
 
-        // 将方法添加到类中
+        //将方法添加到类中
         cc.addMethod(method);
 
-        // 将类写入文件
-        cc.writeFile("path/to/output/directory"); // 指定输出目录
+        /**
+         * 如果有需要,将类写入指定目录
+         * cc.writeFile("javassist/out");
+         */
 
-        // 加载并实例化动态生成的类
+        //加载并实例化动态生成的类
         Class<?> dynamicClass = cc.toClass();
         Object instance = dynamicClass.getDeclaredConstructor().newInstance();
-
-        // 调用动态方法
-        String result = (String) dynamicClass.getMethod("sayHello", String.class)
+        //调用动态方法
+        String result = (String) dynamicClass
+                .getMethod("sayHello", String.class)
                 .invoke(instance, "World");
 
-        // 输出 "Hello, World"
+        //输出 "Hello, World"
         System.out.println(result);
     }
 
