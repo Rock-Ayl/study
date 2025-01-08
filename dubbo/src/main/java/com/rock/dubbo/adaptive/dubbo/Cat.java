cebefeaf5c606092ab5a6cc367bf7f1fa930b5fc
Rock-Ayl <AnYongLiang4869@163.com>
2025-01-08 11:46:01 +0800

    修改：dubbo active

--- dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Cat.java
+++ dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Cat.java
@@ -1,6 +1,7 @@
 package com.rock.dubbo.adaptive.dubbo;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.Activate;
 
 /**
  * 实现 猫
@@ -8,6 +9,8 @@ import org.apache.dubbo.common.URL;
  * @Author ayl
  * @Date 2024-12-26
  */
+//指定其为 Activate 类、并指定分组
+@Activate(group = "分组1")
 public class Cat implements Animal {
 
     @Override
--- dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Start.java
+++ dubbo/src/main/java/com/rock/dubbo/adaptive/dubbo/Start.java
@@ -3,6 +3,8 @@ package com.rock.dubbo.adaptive.dubbo;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 
+import java.util.List;
+
 /**
  * @Author ayl
  * @Date 2024-12-26
@@ -10,6 +12,11 @@ import org.apache.dubbo.common.extension.ExtensionLoader;
 public class Start {
 
     public static void main(String[] args) {
+
+        /**
+         * Dubbo SPI{@link ExtensionLoader} 本质是代替了 Java SPI 的 {@link java.util.ServiceLoader}
+         */
+
         //载入
         ExtensionLoader<Animal> loader = ExtensionLoader.getExtensionLoader(Animal.class);
 
@@ -22,8 +29,10 @@ public class Start {
         loader.getExtension("dog").eat(null);
 
         /**
-         * spi + 自适配扩展 实现
+         * spi + 自适配扩展{@link org.apache.dubbo.common.extension.Adaptive} 实现
          */
+
+        System.out.println("############# spi + 自适配扩展 实现 ################");
 
         //没有值，默认dog
         loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx"));
@@ -31,6 +40,26 @@ public class Start {
         loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx?animalType=cat"));
         //匹配另一个字段，加载cat
         loader.getAdaptiveExtension().eat(URL.valueOf("http://localhost:9999/xxx?animalType2=cat"));
+
+        /**
+         * spi + {@link org.apache.dubbo.common.extension.Activate} 实现
+         */
+
+        System.out.println("############# spi + active 实现 ################");
+
+        //获取所有 符合条件 的可用spi
+        List<Animal> activateExtensionList = loader.getActivateExtension(
+                URL.valueOf("http://localhost:9999/xxx?animalType=dog"),
+                //不考虑group,满足url的参数,即使不是Activate类,也可以获取到
+                "animalType",
+                //不考虑key,满足分组,即算符合条件
+                "分组1"
+        );
+        //循环
+        for (Animal animal : activateExtensionList) {
+            //实现
+            animal.eat(URL.valueOf("http://localhost:9999/xxx?animalType=dog"));
+        }
 
     }
 
