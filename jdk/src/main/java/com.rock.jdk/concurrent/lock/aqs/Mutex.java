c7dd974a03cbdbe22db4948cae8bb3ced0e0e61c
Rock-Ayl <AnYongLiang4869@163.com>
2025-01-16 11:27:47 +0800

    修改：学习AQS-2

--- jdk/src/main/java/com.rock.jdk/concurrent/lock/aqs/Mutex.java
+++ jdk/src/main/java/com.rock.jdk/concurrent/lock/aqs/Mutex.java
@@ -6,54 +6,93 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
  * @Author ayl
  * @Date 2025-01-16
  */
-public class Mutex implements java.io.Serializable {
+public class Mutex {
 
-    //静态内部类，继承AQS
+    /**
+     * 实现AQS
+     */
     private static class Sync extends AbstractQueuedSynchronizer {
 
-        //是否处于占用状态
+        /**
+         * 是否处于占用状态
+         *
+         * @return
+         */
         protected boolean isHeldExclusively() {
             return getState() == 1;
         }
 
-        //当状态为0的时候获取锁，CAS操作成功，则state状态为1，
+        /**
+         * 当状态为0的时候获取锁，CAS操作成功，则state状态为1，
+         *
+         * @param acquires
+         * @return
+         */
         public boolean tryAcquire(int acquires) {
+            //cas操作
             if (compareAndSetState(0, 1)) {
                 setExclusiveOwnerThread(Thread.currentThread());
+                //是
                 return true;
             }
+            //否
             return false;
         }
 
-        //释放锁，将同步状态置为0
+        /**
+         * 释放锁，将同步状态置为0
+         *
+         * @param releases
+         * @return
+         */
         protected boolean tryRelease(int releases) {
-            if (getState() == 0) throw new IllegalMonitorStateException();
+            //如果已经释放了
+            if (getState() == 0) {
+                //抛出异常
+                throw new IllegalMonitorStateException();
+            }
             setExclusiveOwnerThread(null);
+            //设置状态
             setState(0);
+            //返回
             return true;
         }
 
     }
 
-    //同步对象完成一系列复杂的操作，我们仅需指向它即可
+    //初始化 同步对象
     private final Sync sync = new Sync();
 
-    //加锁操作，代理到acquire（模板方法）上就行，acquire会调用我们重写的tryAcquire方法
+    /**
+     * 获取锁
+     */
     public void lock() {
-        sync.acquire(1);
+        this.sync.acquire(1);
     }
 
+    /**
+     * 尝试获取锁
+     *
+     * @return
+     */
     public boolean tryLock() {
-        return sync.tryAcquire(1);
+        return this.sync.tryAcquire(1);
     }
 
-    //释放锁，代理到release（模板方法）上就行，release会调用我们重写的tryRelease方法。
+    /**
+     * 释放锁
+     */
     public void unlock() {
-        sync.release(1);
+        this.sync.release(1);
     }
 
+    /**
+     * 判断是否被获取锁了
+     *
+     * @return
+     */
     public boolean isLocked() {
-        return sync.isHeldExclusively();
+        return this.sync.isHeldExclusively();
     }
 
 }
