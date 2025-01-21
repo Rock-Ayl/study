f6de78ad6c7832e1078d2fc9e4b56e72a8e98277
Rock-Ayl <AnYongLiang4869@163.com>
2025-01-21 10:29:17 +0800

    修改：一个帮别人的计算

--- jdk/src/main/java/com.rock.jdk/test/Test.java
+++ jdk/src/main/java/com.rock.jdk/test/Test.java
@@ -2,8 +2,8 @@ package com.rock.jdk.test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @Author ayl
@@ -70,15 +70,19 @@ public class Test {
     }
 
     private static void findAndPrintSolutions(BigDecimal A, BigDecimal B, BigDecimal C, BigDecimal D) {
-        List<BigDecimal[]> solutions = findSolutions(A, B, C, D);
-        for (BigDecimal[] solution : solutions) {
-            System.out.printf("x = %.2f, y = %.2f, z = %.2f\n", solution[0], solution[1], solution[2]);
+        //计算
+        Set<Node> solutions = findSolutions(A, B, C, D);
+        //循环
+        for (Node solution : solutions) {
+            //输出
+            System.out.printf("x = %.2f, y = %.2f, z = %.2f\n", solution.x, solution.y, solution.z);
         }
     }
 
-    private static List<BigDecimal[]> findSolutions(BigDecimal A, BigDecimal B, BigDecimal C, BigDecimal D) {
+    //具体单个实现
+    private static Set<Node> findSolutions(BigDecimal A, BigDecimal B, BigDecimal C, BigDecimal D) {
         //结果列表
-        List<BigDecimal[]> solutions = new ArrayList<>();
+        Set<Node> solutions = new HashSet<>();
         //每次叠加
         BigDecimal step = new BigDecimal("0.00001");
         //x、y、z的最大值
@@ -98,13 +102,16 @@ public class Test {
                         .setScale(2, RoundingMode.HALF_UP);
                 //目标结果
                 BigDecimal rightSide = D.setScale(2, RoundingMode.HALF_UP);
-                //如果满足
+                //如果满足误差
                 if (leftSide.subtract(rightSide).abs().compareTo(tolerance) < 0) {
-                    //记录结果
-                    solutions.add(new BigDecimal[]{
+                    //初始化结果
+                    Node node = new Node(
                             x.setScale(2, RoundingMode.HALF_UP),
                             y.setScale(2, RoundingMode.HALF_UP),
-                            z.setScale(2, RoundingMode.HALF_UP)});
+                            z.setScale(2, RoundingMode.HALF_UP)
+                    );
+                    //记录结果
+                    solutions.add(node);
                     //如果结果够了
                     if (solutions.size() >= 50) {
                         //结束
