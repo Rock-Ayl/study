0d82bda80580588680bd3724cf0eca82bad313b3
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-12 16:12:20 +0800

    修改：facebook 登录-2

--- api/src/main/java/com/rock/api/facebook/OauthLoginDemo.java
+++ api/src/main/java/com/rock/api/facebook/OauthLoginDemo.java
@@ -2,6 +2,11 @@ package com.rock.api.facebook;
 
 /**
  * Oauth 登录
+ * step 1. [facebook 账号] + [facebook 开发者账号]
+ * step 2. [facebook 开发者账号] 构建 app
+ * step 2.1 地址：https://developers.facebook.com/apps
+ * step 2.2 选择用例：[facebook登录来验证用户身份]
+ * step 2.3 基于app,创建[测试app]用户开发
  *
  * @Author ayl
  * @Date 2025-03-12
