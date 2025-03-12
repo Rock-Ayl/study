cbb1e62ad9864d972e768e855482c38a10901361
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-12 15:56:04 +0800

    修改：facebook 登录-1

---
+++ api/src/main/java/com/rock/api/facebook/OauthLoginDemo.java
@@ -0,0 +1,15 @@
+package com.rock.api.facebook;
+
+/**
+ * Oauth 登录
+ *
+ * @Author ayl
+ * @Date 2025-03-12
+ */
+public class OauthLoginDemo {
+
+    public static void main(String[] args) {
+        //todo
+    }
+
+}
